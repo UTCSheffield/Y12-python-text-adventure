1edec2e460f90e34bc28fd884292e38d3b16e570
Martyn Eggleton <martyn.eggleton@gmail.com>
2025-10-20 13:12:13 +0000

    rename tile_exists to get_tile

--- world.py
+++ world.py
@@ -1,30 +1,35 @@
 __author__ = 'Phillip Johnson'
+__author__ = 'Mr Eggleton'
+# __author__ = ''
 
 _world = {}
 starting_position = (0, 0)
 
-def tile_exists(x, y):
-        """Returns the tile at the given coordinates or None if there is no tile.
 
-        :param x: the x-coordinate in the worldspace
-        :param y: the y-coordinate in the worldspace
-        :return: the tile at the given coordinates or None if there is no tile
-        """
-        return _world.get((x, y))
+def get_tile(x, y):
+    """Returns the tile at the given coordinates or None if there is no tile.
+
+    :param x: the x-coordinate in the worldspace
+    :param y: the y-coordinate in the worldspace
+    :return: the tile at the given coordinates or None if there is no tile
+    """
+    return _world.get((x, y))
 
 
 def load_tiles():
     """Parses a file that describes the world space into the _world object"""
     with open('resources/map.txt', 'r') as f:
         rows = f.readlines()
-    x_max = len(rows[0].split('\t'))
+    x_max = len(rows[0].split('\t'))  # Splitting up the map with Tabs
     for y in range(len(rows)):
-        cols = rows[y].split('\t')
+        cols = rows[y].split('\t')  # Splitting up the map with Tabs
         for x in range(x_max):
-            tile_name = cols[x].replace('\n', '')
-            if tile_name == 'StartingRoom':
+            tile_name = cols[x].replace('\n', '')  # stripping off newlines
+            if tile_name == 'Start':  # record the Start position
                 global starting_position
                 starting_position = (x, y)
-            _world[(x, y)] = None if tile_name == '' else getattr(__import__('tiles'), tile_name)(x, y)
-
-
+            if tile_name == '':
+                _world[(x, y)] = None  # If no room mark the map space blank
+            else:
+                # Create a Tile object for the Class in the map at (x, y)
+                _world[(x, y)] = getattr(__import__('tiles'), tile_name)(x, y)
